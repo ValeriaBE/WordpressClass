e0b49a1ac50c5db9c51b9a7e86313e5a1ffe1d80
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2020-05-17 14:19:24 -0500

    template base

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WordpressClass
 Repositorio de las clases de Wordpress
+1. Clonar template base
+2. Download Local
+3. Crear site en Local, PHP 7.3
+4. Incluir en wp-content themes el archivo del template base
+5. Login the website
---
+++ template-wordpress-master/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+<meta name="google-site-verification" content="cmXC_4jm86FLs_UKqxxlFXcWcxIWMlis5wD5YB64YYw" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>">
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700|Montserrat:300,400,500,600,700&display=swap" rel="stylesheet">
+	<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png">
+	<title><?php wp_title(''); ?> <?php if(wp_title('',false)){echo':';} ?> <?php bloginfo('name') ?></title>
+	<meta name="theme-color" content="#ec6a03"/>
+	<meta name="msapplication-navbutton-color" content="#ec6a03"/>
+	<?php  wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="page">
+		<div id="contenido" >
